006b27894a297f55768dd3a1ed50b1257a3ca5db
Tomas <tomasloha@gmail.com>
2023-10-30 16:38:25 -0300

    1.0.3

--- src/main/java/enumeraciones/Enum.java
+++ src/main/java/enumeraciones/Enum.java
@@ -2,9 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package controllers;
+package enumeraciones;
 
-
-public class PortalControlador {
+/**
+ *
+ * @author oscarloha
+ */
+public class Enum {
     
 }
